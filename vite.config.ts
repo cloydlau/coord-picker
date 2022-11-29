b931c17f229d4c80fb2b628ca972756416d4c28f
cloydlau <253055734@qq.com>
2022-11-29 10:50:41 +0800

    build: add sourcemap

--- vite.config.ts
+++ vite.config.ts
@@ -54,6 +54,7 @@ export default defineConfig({
       name,
       entry: 'src/index.ts',
     },
+    sourcemap: true,
     rollupOptions: {
       external: ['element-ui', 'vue'],
       output: {
